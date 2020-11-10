8388fc9c5043871a562ddf561c10d801dce7f3e5
Sean Mattson <mattsos2@gmatc.matc.edu>
2020-11-09 22:52:34 -0600

    added Create endpoint

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -4,8 +4,9 @@ using Application.Posts;
 using Domain;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-
-
+using System;
+using Persistence;
+using System.Linq;
 
 namespace API.Controllers
 {
@@ -17,12 +18,62 @@ namespace API.Controllers
     {
 
         private readonly IMediator mediator;
+        private readonly DataContext context;
 
-        public PostsController(IMediator mediator) => this.mediator = mediator;
+        public PostsController(IMediator mediator, DataContext context) 
+        {
+            this.mediator = mediator;
+            this.context = context;
+        } 
 
         public async Task<ActionResult<List<Post>>> List()
         {
             return await this.mediator.Send(new List.Query());
         }
+
+        /// <summary>
+        /// GET api/posts
+        /// </summary>
+        /// <returns>A list of posts</returns>
+        [HttpGet]
+        public ActionResult<List<Post>> Get()
+        {
+            return this.context.Posts.ToList();
+        }
+
+        /// <summary>
+        /// GET api/post/[id]
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>A single post</returns>
+        [HttpGet("{id}")]
+        public ActionResult<Post> GetById(Guid id)
+        {
+            return this.context.Posts.Find(id);
+        }
+
+        [HttpPost]
+        public ActionResult<Post> Create([FromBody]Post request)
+        {
+            var post = new Post
+            {
+                Id = request.Id,
+                Title = request.Title,
+                Body = request.Body,
+                Date = request.Date
+            };
+
+            context.Posts.Add(post);
+            var success = context.SaveChanges() > 0;
+
+            if(success)
+            {
+                return post;
+            }
+
+            throw new Exception("Error creating post");
+
+        }
+
     }
 }
